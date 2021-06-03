d17b3e63cffde4f66b389689215d76d3496301ee
phamtanphat <phatdroid94@gmail.com>
2021-06-03 18:48:09 +0700

    promise

--- index.js
+++ index.js
@@ -15,8 +15,33 @@ const request = require('request');
 //     console.log(error || temp);
 // })
 
-function cong(a , b , cb){
-    const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`;
+// function cong(a , b , cb){
+//     const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`;
+//     request(URL, { json: true }, function (error, response, body) {
+//         if (error) return cb(error);
+//         if (!body.success) return cb(body.message)
+//         return cb(null, body.message);
+//     });
+// }
+
+function cong(a, b) {
+    return new Promise((resolve , reject) => {
+        const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`;
+        request(URL, { json: true }, function (error, response, body) {
+            if (error) return reject(error);
+            if (!body.success) return reject(body.message)
+            return resolve(body.message);
+        });
+    })
+}
+
+
+cong(3,2)
+.then(tong => console.log(tong))
+.catch(error => console.log(error))
+
+function tru(a, b, cb){
+    const URL = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`;
     request(URL, { json: true }, function (error, response, body) {
         if (error) return cb(error);
         if (!body.success) return cb(body.message)
@@ -24,15 +49,14 @@ function cong(a , b , cb){
     });
 }
 
-function tru(a, b, cb){
-    const URL = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`;
-    request(URL, { json: true }, function (error, response, body) {
-        if (error) return callback(error);
-        if (!body.success) return callback(body.message)
-        return callback(null, body.message);
-    });
-}
+// 3 + 2 - 5
 
-cong(3, "a", (error , tong) => {
-    console.log(error || tong)
-})
+// cong(3 , 2 , (error , tong) => {
+//     if (error) return console.log(error);
+//     tru(tong , 5 , (error , hieu) => {
+//         if (error) return console.log(error);
+//         console.log(hieu);
+//     })
+// })
+
+// promise
